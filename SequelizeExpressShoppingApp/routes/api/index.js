d60d4e2cb89948ca3328d6d356b3ab254718ed70
Ayush Paul <ayushpaul2002@gmail.com>
2021-10-17 01:46:04 +0530

    added benefits of routing comment

--- SequelizeExpressShoppingApp/routes/api/index.js
+++ SequelizeExpressShoppingApp/routes/api/index.js
@@ -1,8 +1,16 @@
 const route = require('express').Router()
 
-route.use('/users', require('./users'))
+route.use('/users', require('./users')) // Gives the functionality of all the GET/POST methods defined in users.js anywhere wwhenever a request is made to /api/users
 route.use('/products', require('./products'))
 route.use('/transactions', require('./transactions'))
 exports = module.exports = {
     route
 }
+
+/* Benefits of Routing :
+1 ) If we do not do routing of various different functions of different entities then we ould have to write redundant code for the same
+functionality ( eg : add_user.js and verify_user.js make the same get request to fetch user data just by calling /api/user) , which we could just have routed from one script file.
+
+2 ) The above problem of having to write redundant code for different applications can be solved by making one centralized script which will hold the get/post methods
+of all the different entities/objects ( here , products and users) but that will make the code unreadable and difficult to de-bug later.
+*/
